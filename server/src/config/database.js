b27c7e768307eb1311596cdadd3bfeb884339c5d
swapnaBommu <swapnabommu8143@gmail.com>
2026-02-17 16:17:42 +0530

    Added mongodb cloud db

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const url = process.env.DB_URL;
+const url = process.env.ENV === 'development' ? process.env.LOCAL_DB_URL : process.env.DB_URL;
 
 export const connectDB = async () => {
     try{
         await mongoose.connect(url);
-        console.log("Mongodb connected using mongoose");
+        console.log("Mongodb connected using mongoose",url);
     }catch(err){
          console.log("Error while connecting to db",err);
     }
